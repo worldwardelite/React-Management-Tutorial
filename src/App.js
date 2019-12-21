c583f280e40b9336431e25f193e4e4a508517578
worldwardelite <KangTa1217@inbox.ru>
2019-12-22 07:33:43 +0800

    comment line

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="gray-background">
       <img src={logo}  alt=""/>
       <h2>Let's develop management system!</h2>
-      
+      {/* comment line */}
     </div>
   );
 }
