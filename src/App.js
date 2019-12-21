d94314bd9e181eab9da8f29cc9d47d3048c0f0a9
worldwardelite <KangTa1217@inbox.ru>
2019-12-22 07:07:51 +0800

    this is first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo}  alt=""/>
+      <h2>Let's develop management system!</h2>
+      
+    </div>
+  );
+}
+
+export default App;
