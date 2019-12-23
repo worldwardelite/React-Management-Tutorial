b0abf43ee7ea417f1a54b56aefe2e944aab9740e
worldwardelite <KangTa1217@inbox.ru>
2019-12-24 05:21:44 +0800

    Add Customer Api Loding View

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
@@ -18,17 +19,23 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress:{
+    marign: theme.spacing.unit *2
   }
+
 })
 
 
 class  App extends Component {
 
   state = {
-    customers: ''
+    customers: '',
+    completed: 0
   }
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then(res => this.setState({customers: res}))
       .catch(err => console.log(err));
@@ -40,6 +47,13 @@ class  App extends Component {
     return body;
   }
 
+  progress = () => {
+    const {completed} = this.state;
+    this.setState({
+      completed: completed >= 100? 0 : completed+1
+    });
+
+  }
   render(){
     const { classes } = this.props;
     return(
@@ -57,7 +71,13 @@ class  App extends Component {
           </TableHead>
           <TableBody>
         {
-         this.state.customers? this.state.customers.map( customer => { return (<Customer key = {customer.id} id={customer.id} img={customer.img} name={customer.name} birthday={customer.birthday} gender={customer.gender} job = {customer.job} />);}) : ""
+         this.state.customers? this.state.customers.map( customer => { return (<Customer key = {customer.id} id={customer.id} img={customer.img} name={customer.name} birthday={customer.birthday} gender={customer.gender} job = {customer.job} />);}) 
+         : 
+         <TableRow>
+           <TableCell colSpan ="6" align = "center">
+             <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+           </TableCell>
+         </TableRow>
         }
         </TableBody>
         </Table>
